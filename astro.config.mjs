dcd2d673f541960f784c652033dbdd7c4937d3e9
Tickspeed <skyruixian@gmail.com>
2024-11-29 21:07:27 +0100

    test 0.2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
+import icon from "astro-icon";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    server: {
+        host: '0.0.0.0'
+      },
+    integrations: [icon()],
+});
